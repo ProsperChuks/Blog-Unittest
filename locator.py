4efd4055087fbc72a352d852cabcfaa36198b9f6
ProsperChuks <prosperc40@gmail.com>
2021-01-25 13:22:53 +0100

    adding homepage element locators

--- locator.py
+++ locator.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 class homePageLocators(object):
 
     FIRST_POST = (By.LINK_TEXT, "How to tackle poor management")
+    SECOND_POST = (By.LINK_TEXT, "Is this how Europe ends?")
+    THIRD_POST = (By.LINK_TEXT, "Coronavirus crisis is trump's time to lead")
+    FOURTH_POST = (By.LINK_TEXT, "‘I’m not happy’: Donald Trump slams China over handling of COVID-19 crisis")
+    FIFTH_POST = (By.LINK_TEXT, "Musicians sing & drive through Berlin to lift people’s spirits amid lockdown")
+    SIXTH_POST = (By.LINK_TEXT, "Apple Gaming Mac")
+
